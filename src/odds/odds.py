3130d5a67215ae7525a85ce90a6c51ce6582358a
Zach Ross <zross@Zachs-MBP.lan>
2024-05-14 15:16:04 -0400

    added last updated to dataframe to track how recently lines shifted

--- src/odds/odds.py
+++ src/odds/odds.py
@@ -20,7 +20,6 @@ def get_odds(sport):
     df['commence_time'] = pd.to_datetime(df['commence_time'])
     df['commence_time'] = df['commence_time'].dt.tz_convert('US/Eastern')
     df['commence_time'] = df['commence_time'].dt.strftime('%Y-%m-%d %I:%M:%S %p')
-
         
     rows = []
     for game in df['bookmakers']:
@@ -30,18 +29,22 @@ def get_odds(sport):
             team2 = game[i]['markets'][0]['outcomes'][1]['name']
             odds_team1 = game[i]['markets'][0]['outcomes'][0]['price']
             odds_team2 = game[i]['markets'][0]['outcomes'][1]['price']
-            rows.append([bookmaker_name, team1, odds_team1, team2, odds_team2])
+            last_update = game[i]['last_update']
+            rows.append([bookmaker_name, team1, odds_team1, team2, odds_team2, last_update])
 
-    df_odds = pd.DataFrame(rows, columns=['Bookmaker', 'Team1', 'Odds_Team1', 'Team2', 'Odds_Team2'])
+    df_odds = pd.DataFrame(rows, columns=['Bookmaker', 'Team1', 'Odds_Team1', 'Team2', 'Odds_Team2', 'Last_Updated'])
 
     merged_df1 = pd.merge(df, df_odds, left_on=['away_team', 'home_team'], right_on=['Team1', 'Team2'], how='left')
     merged_df2 = pd.merge(df, df_odds, left_on=['home_team', 'away_team'], right_on=['Team1', 'Team2'], how='left')
     merged_df = pd.concat([merged_df1, merged_df2], ignore_index=True)
 
-
     merged_df.dropna(subset=['Team1', 'Team2'], inplace=True)
     merged_df.drop(['id', 'bookmakers', 'Team1', 'Team2'], axis=1, inplace=True)
 
+    #EST Last Updated
+    merged_df['Last_Updated'] = pd.to_datetime(merged_df['Last_Updated'])
+    merged_df['Last_Updated'] = merged_df['Last_Updated'].dt.tz_convert('US/Eastern')
+    merged_df['Last_Updated'] = merged_df['Last_Updated'].dt.strftime('%Y-%m-%d %I:%M:%S %p')
 
     return merged_df
 
@@ -68,7 +71,9 @@ def calculate_margins(dataframe):
                     'Odds_Team1': group.iloc[i]['Odds_Team1'],
                     'Odds_Team2': group.iloc[j]['Odds_Team2'],
                     'Bookmaker1': group.iloc[i]['Bookmaker'],
+                    'Bookmaker1_LastUpdated': group.iloc[i]['Last_Updated'],
                     'Bookmaker2': group.iloc[j]['Bookmaker'],
+                    'Bookmaker2_LastUpdated': group.iloc[j]['Last_Updated'], # Added 'Last_Updated' to 'Bookmaker2_LastUpdated``
                     'Home Team': group.iloc[i]['home_team'],
                     'Away Team': group.iloc[j]['away_team'],
                 })        
